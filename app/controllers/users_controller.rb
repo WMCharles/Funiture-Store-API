01cbb0393aab702de64bd299c4357cb832bcdfce
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-03 13:00:29 +0300

    add user signup actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+    # POST /users
+    def create 
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    # Staying in a session
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user 
+          render json: user
+        else
+          render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+    # Private methods
+    private
+
+    def user_params
+        params.permit(:username, :email, :password, :password_confirmation)
+    end
 end

8850f3b3bcc7d01b76d2300a255200b1cf0c1495
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-03 11:47:35 +0300

    change validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
     validates :name, inclusion: { in: %w(Living-Room Dining Kitchen Bedroom), message: "%{value} is not a valid size" }
-    validates :description, length: { minimum: 50 }
+    validates :description, length: { minimum: 20 }
 end

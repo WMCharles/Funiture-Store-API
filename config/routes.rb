c977bccc6a546a24e99948a724bc2fea5a13dbee
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-03 12:47:40 +0300

    add user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
@@ -6,4 +7,9 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show, :create, :update, :destroy]
   resources :categories, only: [:index, :show, :create, :update, :destroy]
   resources :product_categories, only: [:index, :show, :create, :update, :destroy]
+
+  # Authentication
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
 end

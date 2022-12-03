ae0344cead3d6874531900bb27c4aa6b2887605c
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-03 10:32:20 +0300

    add product controller actions
    configure its routes too

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,53 @@
 class ProductsController < ApplicationController
+
+    #Error messages
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from ActiveRecord::RecordInvalid, with: :render_422
+
+    # GET /products
+    def index 
+        products = Product.all 
+        render json: products, status: :ok
+    end
+
+    # GET /products/:id
+    def show
+        product = Product.find(params[:id])
+        render json: product, status: :ok
+    end
+
+    # POST /products
+    def create
+        product = Product.create!(product_params)
+        render json: product, status: :created
+    end
+
+    # PUT /products/:id
+    def update
+        product = Product.find(params[:id])
+        product.update!(product_params)
+        render json: product, status: :created
+    end
+
+    # DELETE /products/:id
+    def destroy
+        product = Product.find(params[:id])
+        product.destroy
+        head :no_content
+    end
+
+    # Private methods
+    private 
+
+    def product_params
+        params.permit(:title, :description, :price)
+    end
+
+    def render_404
+        render json: { error: "Product not found" }, status: :not_found
+    end
+
+    def render_422(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end

76ab8bf237c931af31d4730aa8ac91cdc54fa64c
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-06 13:24:33 +0300

    alter schema && seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_091822) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_03_103047) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -18,22 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_091822) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "product_categories", force: :cascade do |t|
-    t.integer "category_id"
-    t.integer "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_product_categories_on_category_id"
-    t.index ["product_id"], name: "index_product_categories_on_product_id"
-  end
-
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.integer "price"
     t.string "image_url"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -50,10 +43,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_091822) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "role"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
 puts " 🌱️ Seeding data..."
 
-products = Product.create([
-    {title: "Wooden Kitchen Cabinet", description: "Wall hanging kitchen cabinets to store items like crockery, cookbooks, pickle jars and spices. Wooden Cabinets made from teakwood or solid wood are sturdy and durable, and will last for years to come", price:"45000", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf_APNED7AIry4E326Pnb7wiyPQj8zyPfEhg&usqp=CAU"},
-    {title: "Ria 4 Seater Dining Set", description: "Wipe clean with a soft, dry cloth, avoiding any harsh household cleaners or chemicals that may damage the finish.", price:"100", image_url: "https://dignity.co.ke/wp-content/uploads/2021/10/Ria-4-Seater-Dining-Set_lv.jpg"},
-    {title: "King Size Bed- 6X6 (DB 40736)", description: "Made with bold style and optimum strength, this platform bed with headboard features wooden slats that provide strong support for your memory foam, latex or spring mattress without the need for a box spring. Crafted with clean lines and a streamlined look, this contemporary modern design looks classic and composed in just about any home.", price:"55995", image_url: "https://dignity.co.ke/wp-content/uploads/2022/05/King-Size-Bed-6X6-DB-40736_lv.jpg"},
-    {title: "Flavia 7 Seater Recliner Sofa- Grey", description: "3 seater with 2 end recliners & Drop down feature. 2 seater recliner with console & 2 cup holders. 1 seater recliner. 1 seater recliner", price:"100,000", image_url: "https://dignity.co.ke/wp-content/uploads/2022/10/Flavia-web.jpg"}
-        
-])
-
 categories = Category.create([
     {name:"Living-Room", description: "Funiture intended for user in the living room."},
     {name:"Kitchen", description: "Pieces of furniture in a kitchen, like cabinets, countertops, sinks, and so forth."},
@@ -15,11 +7,12 @@ categories = Category.create([
     {name:"Bedroom", description: "Funiture intended for user in the bedroom."}
 ])
 
-product_categories = ProductCategory.create([
-    {product_id:1, category_id:2},
-    {product_id:2, category_id:3},
-    {product_id:3, category_id:4},
-    {product_id:4, category_id:1}
+products = Product.create([
+    {title: "Wooden Kitchen Cabinet", description: "Wall hanging kitchen cabinets to store items like crockery, cookbooks, pickle jars and spices. Wooden Cabinets made from teakwood or solid wood are sturdy and durable, and will last for years to come", price:"45000", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf_APNED7AIry4E326Pnb7wiyPQj8zyPfEhg&usqp=CAU", category_id:2},
+    {title: "Ria 4 Seater Dining Set", description: "Wipe clean with a soft, dry cloth, avoiding any harsh household cleaners or chemicals that may damage the finish.", price:"100", image_url: "https://dignity.co.ke/wp-content/uploads/2021/10/Ria-4-Seater-Dining-Set_lv.jpg", category_id:3},
+    {title: "King Size Bed- 6X6 (DB 40736)", description: "Made with bold style and optimum strength, this platform bed with headboard features wooden slats that provide strong support for your memory foam, latex or spring mattress without the need for a box spring. Crafted with clean lines and a streamlined look, this contemporary modern design looks classic and composed in just about any home.", price:"55995", image_url: "https://dignity.co.ke/wp-content/uploads/2022/05/King-Size-Bed-6X6-DB-40736_lv.jpg", category_id:4},
+    {title: "Flavia 7 Seater Recliner Sofa- Grey", description: "3 seater with 2 end recliners & Drop down feature. 2 seater recliner with console & 2 cup holders. 1 seater recliner. 1 seater recliner", price:"100,000", image_url: "https://dignity.co.ke/wp-content/uploads/2022/10/Flavia-web.jpg", category_id:1}
+        
 ])
 
 users = User.create([
@@ -28,10 +21,10 @@ users = User.create([
 ])
 
 reviews = Review.create([
-    {rating:3, comment: "How much do you change?", product_id:1, user_id:2},
-    {rating:4, comment: "How much do you change for delivery outside Nairobi?", product_id:2, user_id:1},
-    {rating:5, comment: "How much do you change for repairs?", product_id:3, user_id:2},
-    {rating:5, comment: "How much do you change for training?", product_id:4, user_id:1}
+    {rating:3, comment: "How much do you charge?", product_id:1, user_id:2},
+    {rating:4, comment: "How much do you charge for delivery outside Nairobi?", product_id:2, user_id:1},
+    {rating:5, comment: "How much do you charge for repairs?", product_id:3, user_id:2},
+    {rating:5, comment: "How much do you charge for training?", product_id:4, user_id:1}
 ])
 
 puts "Done Seeding 🍂️"

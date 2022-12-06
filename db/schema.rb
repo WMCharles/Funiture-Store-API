8295f8e3760820bd0a04743a830a9db9ccc657c1
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-06 12:23:03 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_103047) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_091822) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_103047) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
   end
 
 end

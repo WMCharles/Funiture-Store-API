891db46e67da675fdeab3b87f95e60ba54d3b0b7
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-06 22:57:15 +0300

    update serializer

--- app/serializers/product_review_serializer.rb
+++ app/serializers/product_review_serializer.rb
@@ -1,5 +1,5 @@
 class ProductReviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :image_url, :price
+  attributes :id, :title, :description, :image_url, :price, :quantity
   belongs_to :category, serializer: ProductCategorySerializer
   has_many :reviews, serializer: ReviewUserSerializer
 end

dc49965bdc07390a49b8076825167b74a9853d6a
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-06 13:48:57 +0300

    add nested ...

--- app/serializers/product_review_serializer.rb
+++ app/serializers/product_review_serializer.rb
@@ -1,4 +1,5 @@
 class ProductReviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :image_url, :price
   has_many :reviews, serializer: ReviewUserSerializer
+  belongs_to :category, serializer: ProductCategorySerializer
 end
